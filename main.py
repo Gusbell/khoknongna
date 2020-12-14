c2cb3aae113975302185aa48780170ce9e4f7c4f
Gusbell <Gusbell@users.noreply.github.com>
2020-12-15 06:56:50 +0700

    new

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #Project Khok Nong Na Model
 def square_meter():
     land1 = float(input("ไร่: "))
+    land = land1*1600
+    return land
+""" 
     land2 = float(input("งาน: "))
     land = (land1*1600) + (land2*400)
     return land
+"""
